deabb3c23e654f8171bed4113da4c11f2e8a8606
Matgorn <m.gorny@selleo.com>
2021-11-23 16:06:02 +0100

    test: setup test coverage for integer-length task

--- tasks/integer-length/integer-length.js
+++ tasks/integer-length/integer-length.js
@@ -1,9 +1,13 @@
-function integerLength(int) {
+export const integerLength = (int) => {
   const intAsString = String(int);
 
   return intAsString.length;
 };
 
-const integerLength2 = (int) => `${int}`.length;
+export const integerLength2 = (int) => `${int}`.length;
 
-const integerLength3 = (int) => ('' + int).length;
+export const integerLength3 = (int) => ('' + int).length;
+
+// Only implementation that returns valid result
+// for numbers that has more than 21 digits
+export const integerLength4 = (int) => Math.ceil(Math.log(int + 1) / Math.LN10);
