add0267607f9e5d01cbb61ea6d4b0ac51f48d48a
Matgorn <m.gorny@selleo.com>
2021-11-24 10:19:20 +0100

    refactor: approach change for integer-difference tests

--- tasks/integer-difference/integer-difference.test.js
+++ tasks/integer-difference/integer-difference.test.js
@@ -1,27 +1,27 @@
 import { integerDifference, integerDifference2 } from "./integer-difference";
 
-// No.1 variant
-test('integerDifference(4, [1, 1, 5, 6, 9, 16, 27]) should return 3', () => {
-  expect(integerDifference(4, [1, 1, 5, 6, 9, 16, 27])).toBe(3);
+describe('integerDifference', () => {
+  it('returns the number of times that the difference provided as first argument between numbers from second argument\'s array appears', () => {
+    // When first argument is equal 4 and second argument equal to [1, 1, 5, 6, 9, 16, 27]
+    expect(integerDifference(4, [1, 1, 5, 6, 9, 16, 27])).toBe(3);
+
+    // When first argument is equal 2 and second argument equal to [1, 1, 3, 3]
+    expect(integerDifference(2, [1, 1, 3, 3])).toBe(4);
+
+    // When first argument is equal 5 and second argument equal to [1, 2, 64, 5, 7, 43, 24, 25, 64, 69]
+    expect(integerDifference(5, [1, 2, 64, 5, 7, 43, 24, 25, 64, 69])).toBe(3);
+  });
 });
 
-test('integerDifference(2, [1, 1, 3, 3]) should return 4', () => {
-  expect(integerDifference(2, [1, 1, 3, 3])).toBe(4);
-});
+describe('integerDifference2', () => {
+  it('returns the number of times that the difference provided as first argument between numbers from second argument\'s array appears', () => {
+    // When first argument is equal 4 and second argument equal to [1, 1, 5, 6, 9, 16, 27]
+    expect(integerDifference2(4, [1, 1, 5, 6, 9, 16, 27])).toBe(3);
 
-test('integerDifference(5, [1, 2, 64, 5, 7, 43, 24, 25, 64, 69]) should return 4', () => {
-  expect(integerDifference(5, [1, 2, 64, 5, 7, 43, 24, 25, 64, 69])).toBe(3);
-});
+    // When first argument is equal 2 and second argument equal to [1, 1, 3, 3]
+    expect(integerDifference2(2, [1, 1, 3, 3])).toBe(4);
 
-// No. 2 variant
-test('integerDifference2(4, [1, 1, 5, 6, 9, 16, 27]) should return 3', () => {
-  expect(integerDifference2(4, [1, 1, 5, 6, 9, 16, 27])).toBe(3);
-});
-
-test('integerDifference2(2, [1, 1, 3, 3]) should return 4', () => {
-  expect(integerDifference2(2, [1, 1, 3, 3])).toBe(4);
-});
-
-test('integerDifference2(5, [1, 2, 64, 5, 7, 43, 24, 25, 64, 69]) should return 4', () => {
-  expect(integerDifference2(5, [1, 2, 64, 5, 7, 43, 24, 25, 64, 69])).toBe(3);
+    // When first argument is equal 5 and second argument equal to [1, 2, 64, 5, 7, 43, 24, 25, 64, 69]
+    expect(integerDifference2(5, [1, 2, 64, 5, 7, 43, 24, 25, 64, 69])).toBe(3);
+  });
 });
