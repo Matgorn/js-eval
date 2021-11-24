040cd92c64ebcaf6b8be3ec78f38b0e9a72b704d
Matgorn <m.gorny@selleo.com>
2021-11-24 10:33:06 +0100

    refactor: approach change for reverse-string tests

--- tasks/reverse-string/reverse-string.test.js
+++ tasks/reverse-string/reverse-string.test.js
@@ -1,46 +1,51 @@
 import { reverseString, reverseString2, reverseString3, reverseString4, reverseString5 } from "./reverse-string";
 
-// No.1 variant
-test('reverseString("Hello World") should return "dlroW olleH"', () => {
-  expect(reverseString("Hello World")).toBe("dlroW olleH");
+describe('reverseString', () => {
+  it('returns reversed string from the function input', () => {
+    // When the input is equal to "Hello World"
+    expect(reverseString("Hello World")).toBe("dlroW olleH");
+
+    // When the input is equal to "To be or not to be"
+    expect(reverseString("To be or not to be")).toBe("eb ot ton ro eb oT");
+  });
 });
 
-test('reverseString("To be or not to be") should return "eb ot ton ro eb oT"', () => {
-  expect(reverseString("To be or not to be")).toBe("eb ot ton ro eb oT");
+describe('reverseString2', () => {
+  it('returns reversed string from the function input', () => {
+    // When the input is equal to "Hello World"
+    expect(reverseString2("Hello World")).toBe("dlroW olleH");
+
+    // When the input is equal to "To be or not to be"
+    expect(reverseString2("To be or not to be")).toBe("eb ot ton ro eb oT");
+  });
 });
 
-// No.2 variant
-test('reverseString2("Hello World") should return "dlroW olleH"', () => {
-  expect(reverseString2("Hello World")).toBe("dlroW olleH");
+describe('reverseString3', () => {
+  it('returns reversed string from the function input', () => {
+    // When the input is equal to "Hello World"
+    expect(reverseString3("Hello World")).toBe("dlroW olleH");
+
+    // When the input is equal to "To be or not to be"
+    expect(reverseString3("To be or not to be")).toBe("eb ot ton ro eb oT");
+  });
 });
 
-test('reverseString2("To be or not to be") should return "eb ot ton ro eb oT"', () => {
-  expect(reverseString2("To be or not to be")).toBe("eb ot ton ro eb oT");
+describe('reverseString4', () => {
+  it('returns reversed string from the function input', () => {
+    // When the input is equal to "Hello World"
+    expect(reverseString4("Hello World")).toBe("dlroW olleH");
+
+    // When the input is equal to "To be or not to be"
+    expect(reverseString4("To be or not to be")).toBe("eb ot ton ro eb oT");
+  });
 });
 
-// No.3 variant
-test('reverseString3("Hello World") should return "dlroW olleH"', () => {
-  expect(reverseString3("Hello World")).toBe("dlroW olleH");
-});
+describe('reverseString5', () => {
+  it('returns reversed string from the function input', () => {
+    // When the input is equal to "Hello World"
+    expect(reverseString5("Hello World")).toBe("dlroW olleH");
 
-test('reverseString3("To be or not to be") should return "eb ot ton ro eb oT"', () => {
-  expect(reverseString3("To be or not to be")).toBe("eb ot ton ro eb oT");
-});
-
-// No.4 variant
-test('reverseString4("Hello World") should return "dlroW olleH"', () => {
-  expect(reverseString4("Hello World")).toBe("dlroW olleH");
-});
-
-test('reverseString4("To be or not to be") should return "eb ot ton ro eb oT"', () => {
-  expect(reverseString4("To be or not to be")).toBe("eb ot ton ro eb oT");
-});
-
-// No.5 variant
-test('reverseString5("Hello World") should return "dlroW olleH"', () => {
-  expect(reverseString5("Hello World")).toBe("dlroW olleH");
-});
-
-test('reverseString5("To be or not to be") should return "eb ot ton ro eb oT"', () => {
-  expect(reverseString5("To be or not to be")).toBe("eb ot ton ro eb oT");
+    // When the input is equal to "To be or not to be"
+    expect(reverseString5("To be or not to be")).toBe("eb ot ton ro eb oT");
+  });
 });
