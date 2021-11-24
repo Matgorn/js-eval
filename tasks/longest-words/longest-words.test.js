2d514eecd02ac0053e6b7cadfe0841617d85c5bf
Matgorn <m.gorny@selleo.com>
2021-11-24 10:26:42 +0100

    refactor: approach change for longest-words tests

--- tasks/longest-words/longest-words.test.js
+++ tasks/longest-words/longest-words.test.js
@@ -1,13 +1,14 @@
 import { longestWords } from "./longest-words";
 
-test('longestWords("You are just an old antidisestablishmentarian") should return ["antidisestablishmentarian"]', () => {
-  expect(longestWords("You are just an old antidisestablishmentarian")).toStrictEqual(['antidisestablishmentarian']);
-});
+describe('longestWords', () => {
+  it('returns array of the longest word or words from the input', () => {
+    // When input equals "You are just an old antidisestablishmentarian"
+    expect(longestWords("You are just an old antidisestablishmentarian")).toStrictEqual(['antidisestablishmentarian']);
 
-test('longestWords("I gave a present to my parents") should return ["present", "parents"]', () => {
-  expect(longestWords("I gave a present to my parents")).toStrictEqual(["present", "parents"]);
-});
+    // When input equals "I gave a present to my parents"
+    expect(longestWords("I gave a present to my parents")).toStrictEqual(["present", "parents"]);
 
-test('longestWords("Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo")should return ["Buffalo"]', () => {
-  expect(longestWords("Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo")).toStrictEqual(["Buffalo"]);
+    // When input equals "Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo"
+    expect(longestWords("Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo")).toStrictEqual(["Buffalo"]);
+  });
 });
