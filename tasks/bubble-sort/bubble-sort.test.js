07603b0710c74f30f797d301af659167d4fecf65
Matgorn <m.gorny@selleo.com>
2021-11-24 10:10:11 +0100

    refactor: approach change for bubble-sort tests

--- tasks/bubble-sort/bubble-sort.test.js
+++ tasks/bubble-sort/bubble-sort.test.js
@@ -1,27 +1,27 @@
 import { bubbleSort, bubbleSort2 } from "./bubble-sort";
 
-// No.1 variant
-test('bubbleSort(564, 343, 56, 5641, 345,2,-3) should return [-3, 2, 56, 343, 345, 564, 5641]', () => {
-  expect(bubbleSort(564, 343, 56, 5641, 345,2,-3)).toStrictEqual([-3, 2, 56, 343, 345, 564, 5641]);
+describe('bubbleSort', () => {
+  it('sorts numbers provided as input by bubble sort approach', () => {
+    // When input is 564, 343, 56, 5641, 345,2,-3
+    expect(bubbleSort(564, 343, 56, 5641, 345,2,-3)).toStrictEqual([-3, 2, 56, 343, 345, 564, 5641]);
+
+    // When input is -1000, 1000, 25, 251, -251, 1, 999
+    expect(bubbleSort(-1000, 1000, 25, 251, -251, 1, 999)).toStrictEqual([-1000, -251, 1, 25, 251, 999, 1000]);
+
+    // When input is -1, 0, 1, -2,-20 , 11
+    expect(bubbleSort(-1, 0, 1, -2,-20 , 11)).toStrictEqual([-20, -2, -1, 0, 1, 11]);
+  });
 });
 
-test('bubbleSort(-1000, 1000, 25, 251, -251, 1, 999) should return [-1000, -251, 1, 25, 251, 999, 1000]', () => {
-  expect(bubbleSort(-1000, 1000, 25, 251, -251, 1, 999)).toStrictEqual([-1000, -251, 1, 25, 251, 999, 1000]);
-});
+describe('bubbleSort2', () => {
+  it('sorts numbers provided as input by bubble sort approach', () => {
+    // When input is 564, 343, 56, 5641, 345,2,-3
+    expect(bubbleSort2(564, 343, 56, 5641, 345,2,-3)).toStrictEqual([-3, 2, 56, 343, 345, 564, 5641]);
 
-test('bubbleSort(-1, 0, 1, -2 ,-20 , 11) should return [-20, -2, -1, 0, 11]', () => {
-  expect(bubbleSort(-1, 0, 1, -2,-20 , 11)).toStrictEqual([-20, -2, -1, 0, 1, 11]);
-});
+    // When input is -1000, 1000, 25, 251, -251, 1, 999
+    expect(bubbleSort2(-1000, 1000, 25, 251, -251, 1, 999)).toStrictEqual([-1000, -251, 1, 25, 251, 999, 1000]);
 
-// No. 2 variant
-test('bubbleSort2(564, 343, 56, 5641, 345,2,-3) should return [-3, 2, 56, 343, 345, 564, 5641]', () => {
-  expect(bubbleSort2(564, 343, 56, 5641, 345,2,-3)).toStrictEqual([-3, 2, 56, 343, 345, 564, 5641]);
-});
-
-test('bubbleSort2(-1000, 1000, 25, 251, -251, 1, 999) should return [-1000, -251, 1, 25, 251, 999, 1000]', () => {
-  expect(bubbleSort2(-1000, 1000, 25, 251, -251, 1, 999)).toStrictEqual([-1000, -251, 1, 25, 251, 999, 1000]);
-});
-
-test('bubbleSort2(-1, 0, 1, -2 ,-20 , 11) should return [-20, -2, -1, 0, 11]', () => {
-  expect(bubbleSort2(-1, 0, 1, -2,-20 , 11)).toStrictEqual([-20, -2, -1, 0, 1, 11]);
+    // When input is -1, 0, 1, -2,-20 , 11
+    expect(bubbleSort2(-1, 0, 1, -2,-20 , 11)).toStrictEqual([-20, -2, -1, 0, 1, 11]);
+  });
 });
