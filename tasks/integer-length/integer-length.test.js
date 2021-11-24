048c81ca82421a1f87d67d6b807b065780997b51
Matgorn <m.gorny@selleo.com>
2021-11-24 10:23:45 +0100

    refactor: approach change for integer-length tests

--- tasks/integer-length/integer-length.test.js
+++ tasks/integer-length/integer-length.test.js
@@ -1,41 +1,44 @@
 import { integerLength, integerLength2, integerLength3, integerLength4 } from "./integer-length";
 
-// No.1 variant
-test('integerLength(3412) should return 20', () => {
-  expect(integerLength(3412)).toBe(4);
+describe('integerLength', () => {
+  it('returns number of digits of the provided number input limited to 21 digits', () => {
+    // When input is equals 3412
+    expect(integerLength(3412)).toBe(4);
+
+    // When input is equals 38173746574837291827
+    expect(integerLength(38173746574837291827)).toBe(20);
+  });
 });
 
-test('integerLength(3817374657483729182749) should return 20', () => {
-  expect(integerLength(38173746574837291827)).toBe(20);
+describe('integerLength2', () => {
+  it('returns number of digits of the provided number input limited to 21 digits', () => {
+    // When input is equals 3412
+    expect(integerLength2(3412)).toBe(4);
+
+    // When input is equals 38173746574837291827
+    expect(integerLength2(38173746574837291827)).toBe(20);
+  });
 });
 
-// No. 2 variant
-test('integerLength2(3412) should return 20', () => {
-  expect(integerLength2(3412)).toBe(4);
+describe('integerLength3', () => {
+  it('returns number of digits of the provided number input limited to 21 digits', () => {
+    // When input is equals 3412
+    expect(integerLength3(3412)).toBe(4);
+
+    // When input is equals 38173746574837291827
+    expect(integerLength3(38173746574837291827)).toBe(20);
+  });
 });
 
-test('integerLength2(3817374657483729182749) should return 20', () => {
-  expect(integerLength2(38173746574837291827)).toBe(20);
-});
+describe('integerLength3', () => {
+  it('returns number of digits of the provided number input', () => {
+    // When input is equals 3412
+    expect(integerLength3(3412)).toBe(4);
 
-// No.3 variant
-test('integerLength3(3412) should return 20', () => {
-  expect(integerLength3(3412)).toBe(4);
-});
+    // When input is equals 38173746574837291827
+    expect(integerLength3(38173746574837291827)).toBe(20);
 
-test('integerLength3(3817374657483729182749) should return 20', () => {
-  expect(integerLength3(38173746574837291827)).toBe(20);
-});
-
-// No.4 variant
-test('integerLength4(3412) should return 20', () => {
-  expect(integerLength4(3412)).toBe(4);
-});
-
-test('integerLength4(3817374657483729182749) should return 20', () => {
-  expect(integerLength4(3817374657483729182749)).toBe(22);
-});
-
-test('integerLength4(38173746574837291827492342347862345982738475392) should return 20', () => {
-  expect(integerLength4(38173746574837291827492342347862345982738475392)).toBe(47);
+    // When input is equals 38173746574837291827492342347862345982738475392
+    expect(integerLength4(38173746574837291827492342347862345982738475392)).toBe(47);
+  });
 });
